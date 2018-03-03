21e579791e9cef63d365d0b1360d6ae62b51eb84
Justin De Guzman <jdgzmn24@hotmail.com>
2018-03-03 02:12:13 -0600

    Added Functions

--- FridgeApp/app/src/main/java/com/recipe/fridge/fridgeapp/MainActivity.java
+++ FridgeApp/app/src/main/java/com/recipe/fridge/fridgeapp/MainActivity.java
@@ -2,14 +2,45 @@ package com.recipe.fridge.fridgeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView editText;
+    private Button btn;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (TextView) findViewById(R.id.searchInst);
+        btn = (Button) findViewById(R.id.searchButton);
+        list = (ListView) findViewById(R.id.searchTags);
+        arrayList = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+
+        list.setAdapter(adapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+
+                arrayList.add(editText.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
+
 
 
 }
